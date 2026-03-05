69b55053c6d5cb5dab8dfdaa17cd66c75487cfe3
ruangbelajarjapan1-cloud <ruangbelajarjapan1@gmail.com>
2026-03-05 17:24:52 +0900

    Update swiper-init.js

--- assets/js/swiper-init.js
+++ assets/js/swiper-init.js
@@ -1,32 +1,24 @@
-// Konfigurasi Carousel / Slideshow
 document.addEventListener("DOMContentLoaded", function() {
     const swiper = new Swiper(".mySwiper", {
         effect: "slide",
-        speed: 800,
+        speed: 1000, 
         spaceBetween: 30,
         grabCursor: true,
         centeredSlides: true,
         loop: true,
-        
-        // Auto slide
         autoplay: {
-            delay: 4000,
+            delay: 5000,
             disableOnInteraction: false,
             pauseOnMouseEnter: true, 
         },
-        
-        // Titik navigasi
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
             dynamicBullets: true,
         },
-        
-        // Responsif
         breakpoints: {
-            320: { slidesPerView: 1.1, spaceBetween: 20 },
-            768: { slidesPerView: 1.5, spaceBetween: 30 },
-            1024: { slidesPerView: 2, spaceBetween: 40 },
+            320: { slidesPerView: 1, spaceBetween: 20 },
+            768: { slidesPerView: 1.1, spaceBetween: 30 }
         }
     });
 });
